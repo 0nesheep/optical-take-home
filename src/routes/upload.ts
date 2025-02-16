5ddd28a5811838a56422eb0ea67504418911b804
Yiyang <liuyiyang0929@gmail.com>
2025-02-17 00:29:10 +0800

    Fix formatting, update package.json scripts

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -4,58 +4,58 @@ import { database } from '../db';
 export const uploadRouter = Router();
 
 uploadRouter.post('/upload', (req: Request, res: Response) => {
-    const data = req.body.file;
-    if (!data) {
-      return res
-        .status(400)
-        .json({ error: 'No data found, please provide a valid csv file' });
+  const data = req.body.file;
+  if (!data) {
+    return res
+      .status(400)
+      .json({ error: 'No data found, please provide a valid csv file' });
+  }
+
+  const dataEntries = data.trim().split(/\r?\n/);
+
+  // First line is the header and will be ignored
+  dataEntries.shift();
+
+  const validEntries = new Map<string, number>();
+
+  for (let i = 0; i < dataEntries.length; i++) {
+    const entry = dataEntries[i].trim();
+    const cols = entry?.split(',');
+
+    if (cols.length !== 2) {
+      // Invalid CSV row with abnormal columns, terminate
+      return res.status(400).json({
+        error: `Row ${i + 2} has an invalid number of columns.`,
+      });
     }
-  
-    const dataEntries = data.trim().split(/\r?\n/);
-  
-    // First line is the header and will be ignored
-    dataEntries.shift();
-  
-    const validEntries = new Map<string, number>();
-  
-    for (let i = 0; i < dataEntries.length; i++) {
-      const entry = dataEntries[i].trim();
-      const cols = entry?.split(',');
-  
-      if (cols.length !== 2) {
-        // Invalid CSV row with abnormal columns, terminate
-        return res.status(400).json({
-          error: `Row ${i + 2} has an invalid number of columns.`,
-        });
-      }
-  
-      const [name, rawSalary] = cols.map((column: string) => column.trim());
-      if (!name) {
-        // Invalid CSV row with invalid names, terminate
-        return res.status(400).json({
-          error: `Row ${i + 2} has an empty name field`,
-        });
-      }
-  
-      const parsedSalary = parseFloat(rawSalary);
-      if (isNaN(parsedSalary)) {
-        // Invalid CSV row with invalid salary amount, terminate
-        return res.status(400).json({
-          error: `Row ${i + 2} has an invalid salary field`,
-        });
-      }
-  
-      if (parsedSalary < 0) {
-        continue;
-      }
-  
-      validEntries.set(name, parsedSalary);
+
+    const [name, rawSalary] = cols.map((column: string) => column.trim());
+    if (!name) {
+      // Invalid CSV row with invalid names, terminate
+      return res.status(400).json({
+        error: `Row ${i + 2} has an empty name field`,
+      });
     }
-  
-    // Only add valid entries if no fatal errors found
-    validEntries.forEach((salary, name) => {
-      database.set(name, salary);
-    });
-  
-    return res.status(200).json({ success: 1 });
+
+    const parsedSalary = parseFloat(rawSalary);
+    if (isNaN(parsedSalary)) {
+      // Invalid CSV row with invalid salary amount, terminate
+      return res.status(400).json({
+        error: `Row ${i + 2} has an invalid salary field`,
+      });
+    }
+
+    if (parsedSalary < 0) {
+      continue;
+    }
+
+    validEntries.set(name, parsedSalary);
+  }
+
+  // Only add valid entries if no fatal errors found
+  validEntries.forEach((salary, name) => {
+    database.set(name, salary);
   });
+
+  return res.status(200).json({ success: 1 });
+});
